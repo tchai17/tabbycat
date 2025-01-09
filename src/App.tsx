3fb2e718812adff096b11c53f371a940c4d502fe
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2025-01-10 00:55:44 +0800

    ✨ feat(ui): integrate `@mantine/core` and custom theming
    Adds `@mantine/core` and associated packages for UI components and theming.
    Introduces custom color palette and typography with `Rubik` font.
    Replaces legacy styles with Mantine provider and components in `App.tsx`.
    Configures PostCSS plugins for Mantine-specific variables and compatibility.
    
    Improves design consistency and scalability of the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
-// import React, { useState } from "react";
-// import "./App.css";
-
+import {
+  MantineProvider,
+  createTheme,
+  MantineColorsTuple,
+} from "@mantine/core";
+import { Title } from "@mantine/core";
 function App() {
   // const [count, setCount] = useState(0);
 
+  const myColor: MantineColorsTuple = [
+    "#e2f8ff",
+    "#cdecff",
+    "#9dd5fd",
+    "#69bdfa",
+    "#40a9f8",
+    "#269df7",
+    "#1197f8",
+    "#0083dd",
+    "#0074c7",
+    "#0064b1",
+  ];
+
+  const theme = createTheme({
+    fontFamily: "Rubik, Roboto",
+    colors: {
+      myColor,
+    },
+  });
+
   return (
-    <>
-      {/* <h1>Vite + React</h1> */}
-      <h1 className="text-black">TabbyCat</h1>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div> */}
-    </>
+    <MantineProvider theme={theme}>
+      <div style={{ backgroundColor: myColor[1], minHeight: "100vh" }}>
+        <Title ta="center" fw={700} order={1}>
+          TabbyCat
+        </Title>
+        {/* <h1 className="text-black">TabbyCat</h1> */}
+      </div>
+    </MantineProvider>
   );
 }
 
